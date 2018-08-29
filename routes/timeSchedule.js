f008ad68226fec061da445d758c6bbdb3f5e5cb5
Sarvottam Patel <geek.patel512@gmail.com>
2018-08-29 14:15:09 +0530

    update schedule added

--- routes/timeSchedule.js
+++ routes/timeSchedule.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-var timesch = require("../models/timeSchedule");
+var timeschedule = require("../models/timeSchedule");
 var express = require("express");
 var router = express.Router();
 var bodyParser = require("body-parser");
@@ -7,38 +7,63 @@ router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
 router.post("/BookSlot", (req, res) => {
-    var timeslot = req.body.timeslot;
+    var timeslot = Date.now();
     var firstName = req.body.firstname;
     var lastname = req.body.lastname;
     var  phonenumber = req.body.phonenumber
     if(!timeslot || !firstName | !lastname |!phonenumber){
         return res.status(422).json({ success: false, msg:'data missing' });
     }
-    timesch.find({TimeSlot:timeslot},function(err,existsschedule){
+    timeschedule.findOne({TimeSlot:timeslot},function(err,existsschedule){
         if(err){
             return res.status(422).json({ success: false, msg:err });
         }
         if(existsschedule){
-            return res.status(200).json({ success: true, msg:existsschedule });
-
+            // console.log('exist',existsschedule)
+            existsschedule.FirstName = firstName;
+            existsschedule.Lastname = lastname;
+            existsschedule.PhoneNumber = phonenumber;
+            existsschedule.save((err,data) =>{
+                if(err){
+                    return res.status(422).json({ success: false, msg:err });
+                }
+                else{
+                    return res.status(200).json({ success: true, msg:data });
+                }
+            })
+        }else{
+            let newSchedule = new timeschedule({
+                TimeSlot:timeslot,
+                FirstName:firstName,
+                Lastname:lastname,
+                PhoneNumber:phonenumber
+            });
+            
+            newSchedule.save((err,data) =>{
+                if(err){
+                    return res.status(422).json({ success: false, msg:err });
+                }
+                if(data){
+                    console.log('good exist',data);
+                    return res.status(200).json({ success: true, msg:data });
+                }
+            })
         }
-        let newSchedule = new timesch({
-            TimeSlot:timeslot,
-            FirstName:firstName,
-            Lastname:lastname,
-            PhoneNumber:phonenumber
-        });
-        newSchedule.save((err,data) =>{
-            if(err){
-                return res.status(422).json({ success: false, msg:err });
-            }
-            if(data){
-                return res.status(200).json({ success: true, msg:data });
-            }
-        })
+    });
+});
+
+router.get("/getslot/:timeslot",(req,res) =>{
+    let timeslot = req.params.timeslot;
+    if(!timeslot){
+        return res.status(422).json({ success: false, msg:'time slot not provided' });
+    }
+    timeschedule.findOne({TimeSlot:timeslot}, function(err,result){
+        if(err){
+            return res.status(422).json({ success: false, msg:err });
+        }
+        return res.status(200).json({ success: true, msg:result });
+
     })
-
-
 })
 
 
